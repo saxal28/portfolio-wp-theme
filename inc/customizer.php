715080a00a03d68cd21f772adf7ca8bae3086108
Alan Sax <alan@lelander.com>
2018-06-20 20:57:55 -0500

    initial

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * alan-wp-portfolio Theme Customizer
+ *
+ * @package alan-wp-portfolio
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function alan_wp_portfolio_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'alan_wp_portfolio_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'alan_wp_portfolio_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'alan_wp_portfolio_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function alan_wp_portfolio_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function alan_wp_portfolio_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function alan_wp_portfolio_customize_preview_js() {
+	wp_enqueue_script( 'alan-wp-portfolio-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'alan_wp_portfolio_customize_preview_js' );
